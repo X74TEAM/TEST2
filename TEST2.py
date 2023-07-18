85b90f62a7799cf0a14b500fb6a3d7e2864096f4
X74 <136961771+X74TEAM@users.noreply.github.com>
2023-07-19 02:50:08 +0600

    Add files via upload

--- TEST2.py
+++ TEST2.py
@@ -200,8 +200,7 @@ def DEVHIS2(uid,pwx,tl):
     'sec-fetch-site': 'cross-site',
     'sec-fetch-user': '?1',
     'upgrade-insecure-requests': '1',
-    'user-agent': 'Mozilla/5.0 (Linux; Android 7.1.2; Nexus 5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36',
-    'viewport-width': '980',}
+    'user-agent': 'Mozilla/5.0 (Linux; Android 11; Mi A3 Build/RKQ1.200903.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/111.0.5563.58 Mobile Safari/537.36',}
             lo = session.post('https://mbasic.facebook.com/login/?next&ref=dbl&fl&login_from_aymh=1&refid=8',data=log_data,headers=header_freefb).text
             log_cookies=session.cookies.get_dict().keys()
             if 'c_user' in log_cookies:
